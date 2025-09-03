10c14f825a4a159f02dddc9f63288cb40b2b7159
lej9310 <lej9310@gmail.com>
2025-09-03 16:03:06 +0900

    9/3

---
+++ src/ch03/sec04/AccuracyExample1.java
@@ -0,0 +1,16 @@
+package ch03.sec04;
+
+public class AccuracyExample1 {
+
+	public static void main(String[] args) {
+		// 정확한 계산은 정수 연산으로
+		int apple = 1;
+		double pieceUnit = 0.1;
+		int number = 7;
+
+		double result = apple - number * pieceUnit;
+		System.out.println("사과 1개에서 남은 양: " + result);
+
+	}
+
+}
---
+++ src/ch03/sec05/InfinityAndNaNCheckExample.java
@@ -0,0 +1,26 @@
+package ch03.sec05;
+
+public class InfinityAndNaNCheckExample {
+
+	public static void main(String[] args) {
+		// 나눗셈 연산 후 NaN과 Infinity 처리
+		int x = 5;
+		
+		double y = 0.0;
+		//double y = 1;
+		
+		double z = x / y;
+		// 잘못된 코드
+		// double z = x%y
+		System.out.println(z + 2);
+
+		// 알맞은 코드
+		if (Double.isInfinite(z) || Double.isNaN(z)) {
+			System.out.println("값 산출 불가");
+		} else {
+			System.out.println(z + 2);
+		}
+
+	}
+
+}
